3fa41bbd82939c6ffa1ea35ec79e36bbf7aabd83
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2022-04-11 19:44:28 +0200

    prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'https://localhost:3000/api',
+  backendUrl: 'https://localhost:3001/api',
   defaultProfilePicture:
     'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
 };
